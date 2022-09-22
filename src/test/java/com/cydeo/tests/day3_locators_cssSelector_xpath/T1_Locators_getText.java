3254acc877e58fbcbd81cf275d397954a8bc186f
adamar <aysun114@hotmail.com>
2022-09-22 10:55:47 -0500

    T1_Locators_getText class added

---
+++ src/test/java/com/cydeo/tests/day3_locators_cssSelector_xpath/T1_Locators_getText.java
@@ -0,0 +1,50 @@
+package com.cydeo.tests.day3_locators_cssSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_Locators_getText {
+
+    public static void main(String[] args) {
+//        1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+//        2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+//        3- Enter incorrect username: “incorrect”
+         // driver.findElement(By.name("USER_LOGIN")).sendKeys("incorrect");
+        WebElement inputUserName = driver.findElement(By.name("USER_LOGIN"));
+        inputUserName.sendKeys("incorrect");
+
+//        4- Enter incorrect password: “incorrect”
+        // Mac: option + enter
+        // windows: alt + enter
+        WebElement inputPassword = driver.findElement(By.name("USER_PASSWORD"));
+        inputPassword.sendKeys("incorrect");
+
+//        5- Click to login button.
+        WebElement loginBtn = driver.findElement(By.className("login-btn"));
+        loginBtn.click();
+
+//        6- Verify error message text is as expected:
+//        Expected: Incorrect login or password
+        WebElement errorMessage = driver.findElement(By.className("errortext"));
+
+         String expectedErrorMessage = "Incorrect login or password";
+         String actualErrorMessage = errorMessage.getText();
+
+         if(actualErrorMessage.equals(expectedErrorMessage)){
+             System.out.println("Error message verification passed!");
+         }else{
+             System.out.println("Error message verification failed!");
+         }
+
+        driver.quit();
+    }
+}
