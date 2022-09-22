8e1065580ef21fd2f77d2a61d3502e459460fe8b
adamar <aysun114@hotmail.com>
2022-09-22 15:30:11 -0500

    T3_getText_cssSelector class added

--- src/test/java/com/cydeo/tests/day3_locators_cssSelector_xpath/T3_getText_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_locators_cssSelector_xpath/T3_getText_cssSelector.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day3_locators_cssSelector_xpath;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class T3_getText_cssSelector {
 
@@ -22,16 +24,25 @@ public class T3_getText_cssSelector {
         // Locate with using cssSelector by value attribute:
        // WebElement logInBtn =driver.findElement(By.cssSelector("input[value='Log In']"));
 
-        // Locate with using cssSelector by class attribute:
-       // WebElement logInBtn = driver.findElement(By.cssSelector("input[class='login-btn']"));
-
         // Locate with using cssSelector by class attribute: with Syntax #2:
        // WebElement logInBtn = driver.findElement(By.cssSelector("input.login-btn"));
 
         // Locate with using cssSelector by onclick attribute
         //WebElement logInBtn = driver.findElement(By.cssSelector("input[onclick=\"BX.addClass(this, 'wait');\"]"));
 
+        // Locate with using cssSelector by class attribute:
+        WebElement logInBtn = driver.findElement(By.cssSelector("input[class='login-btn']"));
 
+        String expectedLoginBtnText = "Log In";
+        String actualLoginBtnText = logInBtn.getAttribute("value");
+
+        if(actualLoginBtnText.equals(expectedLoginBtnText)){
+            System.out.println("Login text verification passed!");
+        }else{
+            System.out.println("Login text verification failed!");
+        }
+
+    driver.quit();
 
     }
 }
