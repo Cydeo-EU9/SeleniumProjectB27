80a4c14946726f672194aa93490dbfebe78c2e56
adamar <aysun114@hotmail.com>
2022-09-22 15:03:21 -0500

    T3_getText_cssSelector class added

---
+++ src/test/java/com/cydeo/tests/day3_locators_cssSelector_xpath/T3_getText_cssSelector.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests.day3_locators_cssSelector_xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class T3_getText_cssSelector {
+
+    public static void main(String[] args) {
+        //1- Open a chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        //3- Verify “Log in” button text is as expected:
+        //Expected: Log In
+
+        // Locate with using cssSelector by type attribite:
+       // WebElement logInBtn = driver.findElement(By.cssSelector("input[type='submit']"));
+
+        // Locate with using cssSelector by value attribute:
+       // WebElement logInBtn =driver.findElement(By.cssSelector("input[value='Log In']"));
+
+        // Locate with using cssSelector by class attribute:
+       // WebElement logInBtn = driver.findElement(By.cssSelector("input[class='login-btn']"));
+
+        // Locate with using cssSelector by class attribute: with Syntax #2:
+       // WebElement logInBtn = driver.findElement(By.cssSelector("input.login-btn"));
+
+        // Locate with using cssSelector by onclick attribute
+        //WebElement logInBtn = driver.findElement(By.cssSelector("input[onclick=\"BX.addClass(this, 'wait');\"]"));
+
+
+
+    }
+}
