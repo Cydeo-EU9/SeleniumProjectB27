9a985d014c31a2abb1a77d8c77cb55ec66277049
adamar <aysun114@hotmail.com>
2022-09-29 14:49:14 -0500

    TestNG_Selenium class added

--- src/test/java/com/cydeo/tests/day5_testNG_dropdown/TestNG_Selenium.java
+++ src/test/java/com/cydeo/tests/day5_testNG_dropdown/TestNG_Selenium.java
@@ -3,20 +3,28 @@ package com.cydeo.tests.day5_testNG_dropdown;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestNG_Selenium {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        // 1. Set up browser
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
 
     @Test
     public void googleTitle(){
-
-        // 1. Set up browser
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //2. Navigate to the google page
         driver.get("https://www.google.com");
@@ -25,7 +33,14 @@ public class TestNG_Selenium {
         String actualTitle = driver.getTitle();
         String expectedTitle = "Google";
 
-        Assert.assertEquals(actualTitle,expectedTitle);
+        Assert.assertEquals(actualTitle,expectedTitle,"Title is not matching!");
 
     }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.quit();
+    }
+
+
 }
