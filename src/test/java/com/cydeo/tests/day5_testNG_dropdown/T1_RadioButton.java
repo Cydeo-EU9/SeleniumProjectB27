8ef75d7c0f8082bd1da25c01a1d6e5526ce41bce
adamar <aysun114@hotmail.com>
2022-09-29 11:07:41 -0500

    T1_RadioButton class added

--- src/test/java/com/cydeo/tests/day5_testNG_dropdown/T1_RadioButton.java
+++ src/test/java/com/cydeo/tests/day5_testNG_dropdown/T1_RadioButton.java
@@ -1,13 +1,16 @@
 package com.cydeo.tests.day5_testNG_dropdown;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class T1_RadioButton {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //        1. Open Chrome browser
         WebDriver driver= WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
@@ -17,10 +20,30 @@ public class T1_RadioButton {
         driver.get("https://practice.cydeo.com/radio_buttons");
 
 //        3. Click to “Hockey” radio button
+        WebElement hockeyButton = driver.findElement(By.xpath("//input[@id='hockey']"));
+        hockeyButton.click();
 
+        System.out.println(hockeyButton.isSelected());
 
 //        4. Verify “Hockey” radio button is selected after clicking.
+         if(hockeyButton.isSelected()){
+             System.out.println("Hockey button is Selected!");
+         }else{
+             System.out.println("Hockey button is not Selected!");
+         }
 
+         // Test all sport radio buttons if they are clickable
+        List<WebElement> sportRadioBtns = driver.findElements(By.xpath("//input[@name='sport']"));
 
+        for (WebElement eachSport : sportRadioBtns) {
+            eachSport.click();
+            if(eachSport.isSelected()){
+                System.out.println(eachSport.getAttribute("id") + "Radio button is selected");
+            }
+
+            Thread.sleep(3000);
+        }
+
+     driver.quit();
     }
 }
