8c41a7914150d8c0edfddf1df604af59335d5dcc
adamar <aysun114@hotmail.com>
2022-09-29 12:06:41 -0500

    T2_StaleElementReferenceException class added

--- src/test/java/com/cydeo/tests/day5_testNG_dropdown/T2_StaleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day5_testNG_dropdown/T2_StaleElementReferenceException.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day5_testNG_dropdown;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,15 +20,31 @@ public class T2_StaleElementReferenceException {
         driver.get("https://practice.cydeo.com/add_remove_elements/");
 
         //3. Click to “Add Element” button
-
+        WebElement addElementBtn = driver.findElement(By.xpath("//button[text()='Add Element']"));
+        addElementBtn.click();
 
         //4. Verify “Delete” button is displayed after clicking.
-
+        WebElement deleteBtn = driver.findElement(By.xpath("//button[@class='added-manually']"));
+        System.out.println("deleteBtn.isDisplayed() = " + deleteBtn.isDisplayed());
 
         //5. Click to “Delete” button.
+        deleteBtn.click();
+
+        //6. Verify “Delete” button is NOT displayed after clicking.
 
 
-        //6. Verify “Delete” button is NOT displayed after clicking. USE
+        // System.out.println("deleteBtn.isDisplayed() = " + deleteBtn.isDisplayed());
+
+        try{
+            System.out.println("deleteBtn.isDisplayed() = " + deleteBtn.isDisplayed());
+
+        }catch (StaleElementReferenceException e){
+
+            System.out.println("Stale Element Reference Exception is thrown and this is already expected!");
+        }
+
+
+       driver.quit();
 
 
     }
