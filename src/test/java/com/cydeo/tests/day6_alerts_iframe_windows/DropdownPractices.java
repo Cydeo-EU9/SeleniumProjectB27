ca98e1b1a1e470ffc528b918b0815880f5f6680c
adamar <aysun114@hotmail.com>
2022-09-30 10:50:22 -0500

    DropdownPractices class added

--- src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/DropdownPractices.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day6_alerts_iframe_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,26 +26,39 @@ public class DropdownPractices {
     }
 
     @Test
-    public void dropdownTask4(){
+    public void dropdownTask4() throws InterruptedException {
 
-
+        Select selectState = new Select(driver.findElement(By.xpath("//select[@id='state']")));
 
 //        3. Select Illinois using visible text
+        selectState.selectByVisibleText("Illinois");
 
+        Thread.sleep(3000);
 
 //        4. Select Virginia using value
-
+        selectState.selectByValue("VA");
 
 //        5. Select California using index
-
+        selectState.selectByIndex(5);
 
 //        6. Verify final selected option is California.
+        String actualFinalOption = selectState.getFirstSelectedOption().getText();
+        String expectedFinalOption = "California" ;
+
+        Assert.assertEquals(actualFinalOption,expectedFinalOption,"Final selected option verification is failed!");
 
 
-//        Use all Select options. (visible text, value, index)
-
+        //finding index number from options
+        int index=-1;
+        for (WebElement eachState : selectState.getOptions()) {
+            index++;
+            if(eachState.getText().equals("California")){
+                System.out.println(index);
+            }
+        }
 
     }
+
 
 
 
