761bcb44d369888ec11bd665b6fe112f39868f51
adamar <aysun114@hotmail.com>
2022-09-30 15:13:02 -0500

    T4_Iframe_Practice class added

--- src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/T4_Iframe_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/T4_Iframe_Practice.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day6_alerts_iframe_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,11 +27,29 @@ public class T4_Iframe_Practice {
     @Test
     public void iframeTask(){
 
+        // switch to iframe with using index number
+        //driver.switchTo().frame(0);
+
+        // switch to iframe with using id value
+        //driver.switchTo().frame("mce_0_ifr");
+
+        // switch to iframe using iframe locator
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
+
 //        4. Assert: “Your content goes here.” Text is displayed.
+        WebElement textArea = driver.findElement(By.xpath("//p"));
 
+        Assert.assertTrue(textArea.isDisplayed());
 
-//        5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor”
+        // If you have nested iframe and if you want to go back to parent iframe you should use parentFrame()
+        //driver.switchTo().parentFrame();
 
+        // We are switching to main html with using defaultContent()
+        driver.switchTo().defaultContent();
+
+//        5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor” is Displayed
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+        Assert.assertTrue(headerText.isDisplayed());
 
 
     }
