41de922ac9a66ab727291cdb82e42f3b2fc0d623
adamar <aysun114@hotmail.com>
2022-09-30 14:13:32 -0500

    T1_Alert_Practice class added

--- src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/T1_Alert_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/T1_Alert_Practice.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day6_alerts_iframe_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,13 +28,24 @@ public class T1_Alert_Practice {
     @Test
     public void alertTask1(){
         //3. Click to “Click for JS Alert” button
-
+        WebElement informationAlertBtn = driver.findElement(By.xpath("//button[@onclick='jsAlert()']"));
+        informationAlertBtn.click();
 
         //4. Click to OK button from the alert
-
+        // To handle JS alerts we have to use Alert for switching driver to alert form
+        Alert alert = driver.switchTo().alert();
+        // accept() method will click OK button
+        alert.accept();
 
         //5. Verify “You successfully clicked an alert” text is displayed.
+        WebElement textMessage = driver.findElement(By.xpath("//p[@id='result']"));
 
+       // Assert.assertTrue(textMessage.isDisplayed());
+
+        String actualTextMessage = textMessage.getText();
+        String expectedtextMessage= "You successfully clicked an alert";
+
+        Assert.assertEquals(actualTextMessage,expectedtextMessage);
 
     }
 
