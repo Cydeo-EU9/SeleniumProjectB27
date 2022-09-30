438adca6802c783295b9c5340b3524c4f73eb94d
adamar <aysun114@hotmail.com>
2022-09-30 10:12:40 -0500

    DropdownPractices class added

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/DropdownPractices.java
@@ -0,0 +1,33 @@
+package com.cydeo.tests.day6_alerts_iframe_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        // 1. Set up browser
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+    }
+
+    @Test
+    public void dropdownTask4(){
+
+
+    }
+
+
+
+
+}
