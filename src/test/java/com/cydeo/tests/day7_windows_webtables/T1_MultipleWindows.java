41407d393bfd0a91f996d6081bf450548a97f2fc
adamar <aysun114@hotmail.com>
2022-10-07 11:09:37 -0500

    T1_MultipleWindows class added

--- src/test/java/com/cydeo/tests/day7_windows_webtables/T1_MultipleWindows.java
+++ src/test/java/com/cydeo/tests/day7_windows_webtables/T1_MultipleWindows.java
@@ -1,11 +1,15 @@
 package com.cydeo.tests.day7_windows_webtables;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_MultipleWindows {
@@ -27,23 +31,45 @@ public class T1_MultipleWindows {
 
         //3. Assert: Title is “Windows”
 
+        //Assert.assertEquals(driver.getTitle(),"Windows");
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle ="Windows";
+        Assert.assertEquals(actualTitle,expectedTitle);
 
         //4. Click to: “Click Here” link
-
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
 
         //5. Click to: “CYDEO” link
-
+        WebElement cydeoLink = driver.findElement(By.xpath("//a[.='CYDEO']"));
+        cydeoLink.click();
 
         //6. Switch to Cydeo page.
+        Set<String> allWindows = driver.getWindowHandles();
 
+        for (String eachWindow : allWindows) {
+            driver.switchTo().window(eachWindow);
+            System.out.println("driver.getTitle() = " + driver.getTitle());
 
+            if(driver.getTitle().equals("Cydeo")){
+                break;
+            }
+        }
 
         //7. Assert: Title is “Cydeo
+
+//        actualTitle = driver.getTitle();
+//        expectedTitle = "Cydeo";
+//        Assert.assertEquals(actualTitle,expectedTitle);
+
+        Assert.assertEquals(driver.getTitle(),"Cydeo");
+
     }
 
     @AfterMethod
     public void tearDown(){
         //driver.close();
-        //driver.quit();
+        driver.quit();
     }
 }
