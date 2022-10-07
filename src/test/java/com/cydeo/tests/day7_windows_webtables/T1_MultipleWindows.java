f3e816aa271282826f01ebfe44d9cf968edd7147
adamar <aysun114@hotmail.com>
2022-10-07 10:26:39 -0500

    T1_MultipleWindows class added

---
+++ src/test/java/com/cydeo/tests/day7_windows_webtables/T1_MultipleWindows.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.day7_windows_webtables;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_MultipleWindows {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        // 1. Set up browser
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/windows");
+
+    }
+
+    @Test
+    public void multiple_windows_test(){
+
+        //3. Assert: Title is “Windows”
+
+
+        //4. Click to: “Click Here” link
+
+
+        //5. Click to: “CYDEO” link
+
+
+        //6. Switch to Cydeo page.
+
+
+
+        //7. Assert: Title is “Cydeo
+    }
+
+}
