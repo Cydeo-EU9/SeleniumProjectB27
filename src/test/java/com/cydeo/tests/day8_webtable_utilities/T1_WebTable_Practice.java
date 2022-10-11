b3a949a66fc520811f209f0b12b03d9d56aa78cb
adamar <aysun114@hotmail.com>
2022-10-11 12:21:45 -0500

    TestBase class added

--- src/test/java/com/cydeo/tests/day8_webtable_utilities/T1_WebTable_Practice.java
+++ src/test/java/com/cydeo/tests/day8_webtable_utilities/T1_WebTable_Practice.java
@@ -29,6 +29,9 @@ public class T1_WebTable_Practice extends TestBase {
         //2. Verify Bob’s name is listed as expected.
         //Expected: “Bob Martin”
 
+        //1. Go to: https://practice.cydeo.com/web-tables
+        driver.get("https://practice.cydeo.com/web-tables");
+
         WebElement bobMartinName =
                  driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']"));
 
