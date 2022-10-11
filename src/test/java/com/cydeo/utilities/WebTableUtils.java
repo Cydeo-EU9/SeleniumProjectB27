094f0e731e2e14a6dce005c467aceb879d50b70a
adamar <aysun114@hotmail.com>
2022-10-11 10:15:52 -0500

    WebTableUtils class added

---
+++ src/test/java/com/cydeo/utilities/WebTableUtils.java
@@ -0,0 +1,12 @@
+package com.cydeo.utilities;
+
+public class WebTableUtils {
+
+    //Method #1 info:
+    //• Name: returnOrderDate ()
+    //• Return type: String
+    //• Arg1: WebDriver driver
+    //• Arg2: String costumerName
+    //This method should accept a costumerName and return the costumer order date as a String.
+
+}
