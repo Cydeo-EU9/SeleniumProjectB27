b696604fa73e0902becdff245fb3b50d8dce32ca
adamar <aysun114@hotmail.com>
2022-10-11 09:58:46 -0500

    T1_WebTable_Practice class added

--- src/test/java/com/cydeo/tests/day8_webtable_utilities/T1_WebTable_Practice.java
+++ src/test/java/com/cydeo/tests/day8_webtable_utilities/T1_WebTable_Practice.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day8_webtable_utilities;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -29,12 +33,28 @@ public class T1_WebTable_Practice {
         //2. Verify Bob’s name is listed as expected.
         //Expected: “Bob Martin”
 
+        WebElement bobMartinName =
+                 driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']"));
 
+        String expectedBobName = "Bob Martin";
+        String actualBobName = bobMartinName.getText();
 
-
-
+        Assert.assertEquals(actualBobName,expectedBobName);
 
         //3. Verify Bob Martin’s order date is as expected
         //Expected: 12/31/2021
+
+        // You can also use this xpath:  //td[.='Bob Martin']/../td[5]
+        WebElement bobMartinOrderDate =
+                driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']/following-sibling::td[3]"));
+
+        Assert.assertEquals(bobMartinOrderDate.getText(),"12/31/2021");
+
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 }
