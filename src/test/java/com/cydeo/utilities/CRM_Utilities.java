12af2cff737954c7ba937cb2ceda71ba34f53ec7
adamar <aysun114@hotmail.com>
2022-10-13 09:57:34 -0500

    CRM_Utilities class added

---
+++ src/test/java/com/cydeo/utilities/CRM_Utilities.java
@@ -0,0 +1,31 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class CRM_Utilities {
+
+    //Method #1 info:
+    //• Name: login_crm()
+    //• Return type: void
+    //• Arg1: WebDriver driver
+    public static void login_crm(WebDriver driver){
+
+
+
+    }
+
+
+
+    //Method #2 info:
+    //• Name: login_crm()
+    //• Return type: void
+    //• Arg1: WebDriver
+    //• Arg2: String username
+    //• Arg3: String password
+    public static void login_crm(WebDriver driver,String username,String password){
+
+
+
+    }
+
+}
