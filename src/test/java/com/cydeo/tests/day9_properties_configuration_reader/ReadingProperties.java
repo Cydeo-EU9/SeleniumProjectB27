f25560d0e74df3892eba64dcb4997e41b68e2464
adamar <aysun114@hotmail.com>
2022-10-13 13:24:08 -0500

    ReadingProperties added

---
+++ src/test/java/com/cydeo/tests/day9_properties_configuration_reader/ReadingProperties.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day9_properties_configuration_reader;
+
+import org.testng.annotations.Test;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_file_test(){
+
+
+
+    }
+}
