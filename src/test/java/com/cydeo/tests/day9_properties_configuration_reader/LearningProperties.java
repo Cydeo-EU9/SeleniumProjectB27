fda03092381f271cc46a4f6d6b5efcf9c36419df
adamar <aysun114@hotmail.com>
2022-10-13 11:25:21 -0500

    LearningProperties class added

---
+++ src/test/java/com/cydeo/tests/day9_properties_configuration_reader/LearningProperties.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day9_properties_configuration_reader;
+
+import org.testng.annotations.Test;
+
+public class LearningProperties {
+
+    @Test
+    public void java_properties_read_test(){
+
+
+
+    }
+}
