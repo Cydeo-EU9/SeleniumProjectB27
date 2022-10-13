e3592cf48466cafecb261154d9ed6bc567e9269a
adamar <aysun114@hotmail.com>
2022-10-13 11:40:09 -0500

    LearningProperties class added

--- src/test/java/com/cydeo/tests/day9_properties_configuration_reader/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day9_properties_configuration_reader/LearningProperties.java
@@ -7,7 +7,11 @@ public class LearningProperties {
     @Test
     public void java_properties_read_test(){
 
-
+        // key = value
+        // getProperty("key") --> "value"
+        System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
+        System.out.println("System.getProperty(\"java.version\") = " + System.getProperty("java.version"));
 
     }
 }
