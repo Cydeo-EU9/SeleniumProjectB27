782ca7b0e6aab5825c329e9ae124280978e6ba9a
adamar <aysun114@hotmail.com>
2022-10-13 09:40:18 -0500

    T1_CRM_Login class added

--- src/test/java/com/cydeo/tests/day9_properties_configuration_reader/T1_CRM_Login.java
+++ src/test/java/com/cydeo/tests/day9_properties_configuration_reader/T1_CRM_Login.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day9_properties_configuration_reader;
 
 import com.cydeo.tests.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T1_CRM_Login extends TestBase {
@@ -12,13 +14,16 @@ public class T1_CRM_Login extends TestBase {
           driver.get("http://login1.nextbasecrm.com/");
 
 //        3. Enter valid username  // hr2@cydeo.com
-
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("hr2@cydeo.com");
 
 //        4. Enter valid password  // UserUser
-
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
 
 //        5. Click to Log In button
-
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@class='login-btn']"));
+        loginBtn.click();
 
 //        6. Verify title is as expected:
 //        Expected: Portal
