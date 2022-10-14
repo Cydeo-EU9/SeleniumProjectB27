ae42abebb493bd58cd2e8e06c04edeb1b6d612c9
adamar <aysun114@hotmail.com>
2022-10-14 10:55:08 -0500

    JavaFaker_Practice class added

--- src/test/java/com/cydeo/tests/day10_javafaker_driver_util/JavaFaker_Practice.java
+++ src/test/java/com/cydeo/tests/day10_javafaker_driver_util/JavaFaker_Practice.java
@@ -10,8 +10,28 @@ public class JavaFaker_Practice {
 
         Faker faker = new Faker();
 
+        System.out.println("faker.name().firstName() = " + faker.name().firstName());
+        System.out.println("faker.name().lastName() = " + faker.name().lastName());
+        System.out.println("faker.name().fullName() = " + faker.name().fullName());
 
+        System.out.println("faker.address().city() = " + faker.address().city());
+        System.out.println("faker.numerify(\"###-###-####\") = " + faker.numerify("###-###-####"));
 
+        System.out.println("faker.numerify(\"773-###-####\") = " + faker.numerify("773-###-####"));
+
+        System.out.println("faker.numerify(\"#####\") = " + faker.numerify("#####"));
+
+        System.out.println("faker.letterify(\"???????\") = " + faker.letterify("???????"));
+
+        System.out.println("faker.bothify(\"##???#??\") = " + faker.bothify("##???#??"));
+
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard());
+
+        System.out.println("faker.finance().creditCard().replaceAll(\"-\",\"\") = " + faker.finance().creditCard().replaceAll("-", ""));
+
+        System.out.println("faker.chuckNorris().fact() = " + faker.chuckNorris().fact());
+
+        System.out.println("faker.chuckNorris().fact().replaceAll(\"Chuck Norris\",\"Saim\") = " + faker.chuckNorris().fact().replaceAll("Chuck Norris", "Saim"));
 
     }
 }
