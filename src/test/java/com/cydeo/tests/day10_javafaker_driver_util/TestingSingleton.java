7ddc2e645d07dc246ba98585a3c83e084af25247
adamar <aysun114@hotmail.com>
2022-10-14 13:56:57 -0500

    TestingSingleton class added

--- src/test/java/com/cydeo/tests/day10_javafaker_driver_util/TestingSingleton.java
+++ src/test/java/com/cydeo/tests/day10_javafaker_driver_util/TestingSingleton.java
@@ -1,6 +1,25 @@
 package com.cydeo.tests.day10_javafaker_driver_util;
 
+import org.testng.annotations.Test;
+
 public class TestingSingleton {
 
+     @Test
+     public void singleton_understand_test(){
 
+      String str1 = Singleton.getWord();
+      System.out.println("str1 = " + str1);
+
+      String str2= Singleton.getWord();
+      System.out.println("str2 = " + str2);
+
+      String str3= Singleton.getWord();
+      System.out.println("str3 = " + str3);
+     }
+
+     @Test
+    public void singleton_understand_test2(){
+         String str4 = Singleton.getWord();
+         System.out.println("str4 = " + str4);
+     }
 }
