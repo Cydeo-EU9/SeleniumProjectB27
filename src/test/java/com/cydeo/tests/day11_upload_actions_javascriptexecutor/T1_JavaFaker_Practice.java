b71d76eec08e3625cafec5d6136e4ca195b9a36b
adamar <aysun114@hotmail.com>
2022-10-17 11:01:45 -0500

    T1_JavaFaker_Practice class added

--- src/test/java/com/cydeo/tests/day11_upload_actions_javascriptexecutor/T1_JavaFaker_Practice.java
+++ src/test/java/com/cydeo/tests/day11_upload_actions_javascriptexecutor/T1_JavaFaker_Practice.java
@@ -1,5 +1,10 @@
 package com.cydeo.tests.day11_upload_actions_javascriptexecutor;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T1_JavaFaker_Practice {
@@ -9,22 +14,33 @@ public class T1_JavaFaker_Practice {
 //        1. Open browser
 //        2. Go to website: https://practice.cydeo.com/registration_form
 
+        // driver --> Driver.getDriver()
+
+          Driver.getDriver().get(ConfigurationReader.getProperty("registration.form.url"));
+
+        Faker faker = new Faker();
 
 //        3. Enter first name
+        WebElement firstname = Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
+        firstname.sendKeys(faker.name().firstName());
 
-
-//        4. Enter last name
+//        4. Enter last name --> HW
 
 //        5. Enter username
+         WebElement username = Driver.getDriver().findElement(By.xpath("//input[@name='username']"));
+        // only using faker is giving me this way --> rosario.murphy
+        // we have to use logic to change the way of returning
+         String userName = faker.name().username().replace(".","");
+         username.sendKeys(userName);
 
 
-//        6. Enter email address
+//        6. Enter email address  :    email.sendKeys(faker.internet().emailAddress())
 
 
-//        7. Enter password
+//        7. Enter password    :      faker.internet().password()
 
 
-//        8. Enter phone number
+//        8. Enter phone number  : faker.numerify("###-###-####)
 
 
 //        9. Select a gender from radio buttons
