1ee6ef46c69392432f88b131e4fe5e4ef250385d
adamar <aysun114@hotmail.com>
2022-10-17 14:30:44 -0500

    T3_Hover_Test class added

--- src/test/java/com/cydeo/tests/day11_upload_actions_javascriptexecutor/T3_Hover_Test.java
+++ src/test/java/com/cydeo/tests/day11_upload_actions_javascriptexecutor/T3_Hover_Test.java
@@ -1,5 +1,8 @@
 package com.cydeo.tests.day11_upload_actions_javascriptexecutor;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T3_Hover_Test {
@@ -7,6 +10,16 @@ public class T3_Hover_Test {
     @Test
     public void  hover_test(){
         //1. Go to https://practice.cydeo.com/hovers
+        Driver.getDriver().get("https://practice.cydeo.com/hovers");
+
+        // Locate all images:
+        WebElement image1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+        WebElement image2 = Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+        WebElement image3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
+
+        WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user1']"));
+        WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user2']"));
+        WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user3']"));
 
 
         //2. Hover over to first image
