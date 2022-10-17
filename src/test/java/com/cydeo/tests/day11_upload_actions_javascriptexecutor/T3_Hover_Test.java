6cce6dc269c6f85c43f4df7cdbedfc7e1f88a308
adamar <aysun114@hotmail.com>
2022-10-17 14:20:20 -0500

    T3_Hover_Test class added

---
+++ src/test/java/com/cydeo/tests/day11_upload_actions_javascriptexecutor/T3_Hover_Test.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests.day11_upload_actions_javascriptexecutor;
+
+import org.testng.annotations.Test;
+
+public class T3_Hover_Test {
+
+    @Test
+    public void  hover_test(){
+        //1. Go to https://practice.cydeo.com/hovers
+
+
+        //2. Hover over to first image
+
+
+        //3. Assert:
+        //a. “name: user1” is displayed
+        //b. “view profile” is displayed
+
+
+        //   4. Hover over to second image
+
+
+        //   5. Assert:
+        //a. “name: user2” is displayed
+        //b. “view profile” is displayed
+
+
+        //   6. Hover over to third image
+
+
+        //   7. Confirm:
+        //a. “name: user3” is displayed
+        //b. “view profile” is displayed
+
+
+    }
+}
