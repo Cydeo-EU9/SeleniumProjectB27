fbf7aa69579b3a1329db2ddf911de0d65f6ad5e7
adamar <aysun114@hotmail.com>
2022-10-17 15:50:19 -0500

    T6_Scroll_JavaScriptExecutor class added

--- src/test/java/com/cydeo/tests/day11_upload_actions_javascriptexecutor/T6_Scroll_JavaScriptExecutor.java
+++ src/test/java/com/cydeo/tests/day11_upload_actions_javascriptexecutor/T6_Scroll_JavaScriptExecutor.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day11_upload_actions_javascriptexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -19,12 +21,20 @@ public class T6_Scroll_JavaScriptExecutor {
         driver.get("https://practice.cydeo.com/infinite_scroll");
 
         //3- Use below JavaScript method and scroll
-
-
+        // window.scrollBy(x,y) --> x is for right/left , y is for up/down
+        JavascriptExecutor js = ((JavascriptExecutor)driver);
+        js.executeScript("window.scrollBy(0,750)");
 
         //a. 750 pixels down 10 times.
-
+        for(int i=0;i<10;i++){
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,750)");
+        }
 
         //b. 750 pixels up 10 times
+        for(int i=0;i<10;i++){
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,-750)");
+        }
     }
 }
