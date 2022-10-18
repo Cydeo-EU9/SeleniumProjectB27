bc1013d5339cb604e8cd0474ed27f8fc311edcda
adamar <aysun114@hotmail.com>
2022-10-18 09:45:08 -0500

    T6_Scroll_JavaScriptExecutor class added

---
+++ src/test/java/com/cydeo/tests/day12_actions_javascriptexecutor/ActionsPractice.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day12_actions_javascriptexecutor;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractice {
+
+    @Test
+    public void task_4_and_5_test(){
+
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/
+
+
+        //3- Scroll down to “Powered by CYDEO”
+
+
+        //4- Scroll using Actions class “moveTo(element)” method
+
+
+
+//        1- Continue from where the Task 4 is left in the same test.
+
+
+
+//        2- Scroll back up to “Home” link using PageUP button
+
+    }
+}
