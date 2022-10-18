0e5e66cfcb9a3f25a08c0f37d6fb517730c3641c
adamar <aysun114@hotmail.com>
2022-10-18 11:10:28 -0500

    T7_JavascriptExecutor_Scroll class added

---
+++ src/test/java/com/cydeo/tests/day12_actions_javascriptexecutor/T7_JavascriptExecutor_Scroll.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests.day12_actions_javascriptexecutor;
+
+import org.testng.annotations.Test;
+
+public class T7_JavascriptExecutor_Scroll {
+
+    @Test
+    public void task7_javascriptexecutor_scroll_test(){
+
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/large
+
+
+        //3- Scroll down to “Cydeo” link
+
+
+        //4- Scroll up to “Home” link
+
+
+
+    }
+}
