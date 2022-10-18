16b22383e56302a76f3bbbdf16f247603030ead7
adamar <aysun114@hotmail.com>
2022-10-18 10:20:22 -0500

    ActionsPractice class added

--- src/test/java/com/cydeo/tests/day12_actions_javascriptexecutor/ActionsPractice.java
+++ src/test/java/com/cydeo/tests/day12_actions_javascriptexecutor/ActionsPractice.java
@@ -1,5 +1,11 @@
 package com.cydeo.tests.day12_actions_javascriptexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ActionsPractice {
@@ -9,21 +15,20 @@ public class ActionsPractice {
 
         //1- Open a chrome browser
         //2- Go to: https://practice.cydeo.com/
+        Driver.getDriver().get("https://practice.cydeo.com/");
 
-
-
-        //3- Scroll down to “Powered by CYDEO”
-
+        // Create an Actions object to use methods coming from Actions class
+        Actions actions = new Actions(Driver.getDriver());
 
         //4- Scroll using Actions class “moveTo(element)” method
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
 
+        actions.moveToElement(cydeoLink).perform();
 
-
-//        1- Continue from where the Task 4 is left in the same test.
-
-
-
+        BrowserUtils.sleep(3);
 //        2- Scroll back up to “Home” link using PageUP button
+          actions.sendKeys(Keys.PAGE_UP,Keys.PAGE_UP,Keys.PAGE_UP).perform();
 
+          Driver.closeDriver();
     }
 }
