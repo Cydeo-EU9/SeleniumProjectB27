0af6557fce5c63db5b8c31375020553f1a25b46b
adamar <aysun114@hotmail.com>
2022-10-20 11:17:27 -0500

    LibraryLoginTest class added

---
+++ src/test/java/com/cydeo/tests/day13_pom_explicitwait/LibraryLoginTest.java
@@ -0,0 +1,24 @@
+package com.cydeo.tests.day13_pom_explicitwait;
+
+import org.testng.annotations.Test;
+
+public class LibraryLoginTest {
+
+    @Test
+    public void required_field_error_message_test(){
+        //1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
+
+
+        //3- Do not enter any information
+        //4- Click to “Sign in” button
+
+
+
+        //5- Verify expected error is displayed:
+        //Expected: This field is required.
+
+
+
+    }
+}
