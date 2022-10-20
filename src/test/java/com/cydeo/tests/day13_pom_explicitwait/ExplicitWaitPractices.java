14bcb4c5a0deccbb3bb763044220091f49962a82
adamar <aysun114@hotmail.com>
2022-10-20 14:50:14 -0500

    ExplicitWaitPractices class added

--- src/test/java/com/cydeo/tests/day13_pom_explicitwait/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13_pom_explicitwait/ExplicitWaitPractices.java
@@ -1,6 +1,22 @@
 package com.cydeo.tests.day13_pom_explicitwait;
 
+import org.testng.annotations.Test;
+
 public class ExplicitWaitPractices {
 
+    @Test
+    public void dynamically_loaded_page_elements_7_test(){
+        //1. Go to https://practice.cydeo.com/dynamic_loading/7
 
+
+        //2. Wait until title is “Dynamic title”
+
+
+
+        //3. Assert: Message “Done” is displayed.
+        //4. Assert: Image is displayed.
+
+
+
+    }
 }
