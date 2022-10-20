c6c4b568a5b366e19d6314f9cc082fbc6fc76e45
adamar <aysun114@hotmail.com>
2022-10-20 11:44:53 -0500

    LibraryLoginTest class added

--- src/test/java/com/cydeo/tests/day13_pom_explicitwait/LibraryLoginTest.java
+++ src/test/java/com/cydeo/tests/day13_pom_explicitwait/LibraryLoginTest.java
@@ -1,5 +1,8 @@
 package com.cydeo.tests.day13_pom_explicitwait;
 
+import com.cydeo.pages.LibraryLoginPage;
+import com.cydeo.utilities.Driver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LibraryLoginTest {
@@ -8,17 +11,32 @@ public class LibraryLoginTest {
     public void required_field_error_message_test(){
         //1- Open a chrome browser
         //2- Go to: https://library1.cydeo.com
-
+        Driver.getDriver().get("https://library1.cydeo.com");
 
         //3- Do not enter any information
         //4- Click to “Sign in” button
+        LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
 
-
+        libraryLoginPage.signInButton.click();
 
         //5- Verify expected error is displayed:
         //Expected: This field is required.
+        Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMsg.isDisplayed());
+
+    }
+
+    @Test
+    public void invalid_email_format_error_message_test(){
+        //1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
 
 
+        //3- Enter invalid email format
+
+
+
+        //4- Verify expected error is displayed:
+        //Expected: Please enter a valid email address.
 
     }
 }
