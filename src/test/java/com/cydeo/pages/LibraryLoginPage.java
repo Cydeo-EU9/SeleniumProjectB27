9c1a0d3645bd46fb76cf316c5f2c35888ba71d83
adamar <aysun114@hotmail.com>
2022-10-20 11:00:22 -0500

    LibraryLoginPage class added

---
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -0,0 +1,33 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LibraryLoginPage {
+
+    public LibraryLoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (id="inputEmail")
+    public WebElement inputUsername;
+
+    @FindBy (xpath="//input[@id='inputPassword']")
+    public WebElement inputPassword;
+
+    @FindBy (xpath = "//button[.='Sign in']")
+    public WebElement signInButton;
+
+    @FindBy (xpath = "//div[.='This field is required.']/div")
+    public WebElement fieldRequiredErrorMsg;
+
+    @FindBy (xpath = "//div[.='Please enter a valid email address.']/div")
+    public WebElement enterInvalidEmailErrorMsg;
+
+    @FindBy (xpath="//div[.='Sorry, Wrong Email or Password']")
+    public WebElement wrongEmailAndPasswordErrorMsg;
+
+
+}
--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -1,22 +1,23 @@
 package com.cydeo.tests.base;
 
-import com.cydeo.utilities.WebDriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
-
 // This class is the parent of all Test classes
+// You will update this class based on what you are keep writing in your most test classes
+// instead writing in all test classes, you can just write in this class and you can extends this class from any class
 public abstract class TestBase {
 
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp(){
-        driver = WebDriverFactory.getDriver("chrome");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
